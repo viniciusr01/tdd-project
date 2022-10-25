6cbd99e845edf5afb5a6ef71e036a66db82149ce
Vinicius Rodrigues <viniciusr02@yahoo.com.br>
2022-10-25 16:13:53 -0300

    Utilizando Selenium para Testar Interações do Usuário

--- lists/tests.py
+++ lists/tests.py
@@ -1,19 +1,55 @@
-from django.urls import resolve
-from django.test import TestCase
-from django.http import HttpRequest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
 
-from lists.views import home_page
+class NewVisitorTest(unittest.TestCase):
 
-class HomePageTest(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def tearDown(self):
+        self.browser.quit()
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+    def test_can_start_a_list_and_retrieve_it_later(self): 
+    
+        # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
+        self.browser.get('http://localhost:8000')
+
+        # Ela nota que o título da página menciona TODO
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('To-Do', header_text)
+
+        # Ela é convidada a entrar com um item TODO imediatamente
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # Ela digita "Estudar testes funcionais" em uma caixa de texto
+        inputbox.send_keys('Estudar testes funcionais')
+
+        # Quando ela aperta enter, a página atualiza, e mostra a lista
+        # "1: Estudar testes funcionais" como um item da lista TODO
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')  
+        self.assertTrue(
+            any(row.text == '1: Estudar testes funcionais' for row in rows)
+        )
+        
+        # Ainda existe uma caixa de texto convidando para adicionar outro item
+        # Ela digita: "Estudar testes de unidade"
+
+        # A página atualiza novamente, e agora mostra ambos os itens na sua lista
+
+        # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
+        # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
+
+        # Ela visita a URL: a sua lista TODO ainda está armazenada
+
+        # Satisfeita, ela vai dormir
+
+if __name__ == '__main__':
+    unittest.main()
