246fcfa7b53655d149b729fbd2cf0a9d8edd4978
Vinicius Rodrigues <viniciusr02@yahoo.com.br>
2022-10-25 23:44:45 -0300

    Novo Projeto: Atualizando o Template

--- lists/views.py
+++ lists/views.py
@@ -6,15 +6,13 @@ def home_page(request):
 
 def view_list(request, list_id):
     my_list = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=my_list)
-    return render(request, 'list.html', {'items': items})
+    return render(request, 'list.html', {'list': my_list})
 
 
 def new_list(request):
     my_list = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=my_list)
     return redirect(f'/lists/{my_list.id}/')
-
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
