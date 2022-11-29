93df4254e2febdb1e7e3a581fcecae967e705897
Vinicius Rodrigues <viniciusr02@yahoo.com.br>
2022-11-29 20:00:54 -0300

    De volta ao teste funcional - PARTE 1

--- lists/views.py
+++ lists/views.py
@@ -7,8 +7,11 @@ def home_page(request):
     return render(request, 'home.html')
 
 def view_list(request, list_id):
-    my_list = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': my_list})
+    list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect(f'/lists/{list_.id}/')
+    return render(request, 'list.html', {'list': list_})
 
 
 def new_list(request):
@@ -23,8 +26,3 @@ def new_list(request):
         return render(request, 'home.html', {"error": error})
     return redirect(f'/lists/{list_.id}/')
 
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')
